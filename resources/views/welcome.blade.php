53194d5507f35ea0b3842be268480d824d305b97
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-24 14:14:53 -0500

    Add ninjas index page and route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ninja Network</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ninja Network</title>
 </head>
 <body>
-    <h1>Welcome to the Ninja Network</h1>
-    <p>Click the button below to view the list of ninjas.</p>
-    <a href="", class"btn">
-        Find Ninjas
-    </a>
+  <h1>Welcome to the Ninja Network</h1>
+  <p>Click the button below to view the list of ninjas.</p>
+  <a href="/ninjas" , class="btn">
+    Find Ninjas
+  </a>
 </body>
 </html>
