6b10a881b40f9ace0e9ba663a2320ba1a8331e93
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-27 11:10:32 -0500

    Add tailwind formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ninja Network</title>
-</head>
-<body>
-  <h1>Welcome to the Ninja Network</h1>
-  <p>Click the button below to view the list of ninjas.</p>
-  <a href="/ninjas" , class="btn">
-    Find Ninjas
-  </a>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ninja Network</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="px-8 py-12 text-center">
+        <h1>Welcome to the Ninja Network</h1>
+        <p>Click the button below to view the list of ninjas.</p>
+        <a href="/ninjas" , class="btn mt-4 inline-block">Find Ninjas</a>
+    </body>
 </html>
