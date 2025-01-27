a873c6185c58852b3d33aa7b6c9c88aa83ebc6db
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-27 12:29:05 -0500

    Add controller to route and connect db

---
+++ app/Http/Controllers/NinjaController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ninja;
+use Illuminate\Http\Request;
+
+class NinjaController extends Controller
+{
+    public function index()
+    {
+        // route --> /ninjas/
+        // fetch all records & pass into the index view
+        $ninjas = Ninja::orderBy('created_at', 'desc')->get();
+        return view('ninjas.index', ['ninjas' => $ninjas]);
+    }
+
+    public function show($id)
+    {
+        // route --> /ninjas/{id}
+        // fetch a single record and pass into show view
+    }
+
+    public function create()
+    {
+        // route --> /ninjas/create
+        // render a create view (with web form) to users
+    }
+
+    public function store()
+    {
+        // --> /ninjas/ (POST)
+        // handle POST request to store a new ninja record in table
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    // public function update()
+    // {
+    //     //
+    // }
+}
