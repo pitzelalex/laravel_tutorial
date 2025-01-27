12b67fd788e87b99e32d2f51624e5ecbfe43bd53
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-27 09:45:08 -0500

    Add prettier and dynamic route

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,13 +1,19 @@
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ninja Network | Home</title>
-</head>
-<body>
-  <h2>Currently Available Ninjas</h2>
-  <ul>
-    <li>Ninjas Here</li>
-  </ul>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ninja Network | Home</title>
+    </head>
+    <body>
+        <h2>Currently Available Ninjas</h2>
+        <p>{{ $greeting }}</p>
+        <ul>
+            <li>
+                <a href="">{{ $ninjas[0]['name'] }}</a>
+            </li>
+            <li>
+                <a href="">{{ $ninjas[1]['name'] }}</a>
+            </li>
+        </ul>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,13 @@ Route::get('/', function () {
 });
 
 Route::get('/ninjas', function () {
-    return view('ninjas.index');
+    $ninjas = [['name' => 'mario', 'skill' => 75, 'id' => '1'], ['name' => 'luigi', 'skill' => 45, 'id' => '2']];
+
+    return view('ninjas.index', ['greeting' => 'hello', 'ninjas' => $ninjas]);
+});
+
+Route::get('/ninjas/{id}', function ($id) {
+    $ninjas = [['name' => 'mario', 'skill' => 75, 'id' => '1'], ['name' => 'luigi', 'skill' => 45, 'id' => '2']];
+
+    return view('ninjas.show', ['id' => $id]);
 });
