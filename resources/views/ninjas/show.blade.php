8368825b5304e9eac311a6c2a1024782227470b5
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-27 15:47:36 -0500

    Update show blade

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    <h2>Ninja Id - {{ $id }}</h2>
+    <h2>{{ $ninja->name }}</h2>
+    <div class="rounded bg-gray-200 p-4">
+        <p>
+            <strong>Skill level:</strong>
+            {{ $ninja->skill }}
+        </p>
+        <p>
+            <strong>about me:</strong>
+            {{ $ninja->bio }}
+        </p>
+    </div>
 </x-layout>
