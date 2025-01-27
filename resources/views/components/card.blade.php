2576ed64dbb0ca103a561ed9f7cada7b9a8183e4
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-27 10:29:27 -0500

    Add dynamic class via props

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,7 @@
-<div class="card">
+@props(['highlight' => false])
+
+{{-- always has class of card but only has class of highlight if highlight is true --}}
+<div @class(['highlight' => $highlight, 'card'])>
     {{ $slot }}
     <a {{ $attributes }} class="btn">View Details</a>
 </div>
--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -9,7 +9,7 @@
     <ul>
         @foreach ($ninjas as $ninja)
             <li>
-                <x-card href="/ninjas/{{ $ninja['id'] }}">
+                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
                     <h3>{{ $ninja['name'] }}</h3>
                 </x-card>
             </li>
